89dba972435f850a7acbcfa418f11cafa8a6615f
abdulsagheer <abdulsagheeras29@gmail.com>
2022-06-19 19:23:08 +0530

    Basic information of strings

---
+++ Strings.js
@@ -0,0 +1,10 @@
+// Strings -> Data type like boolean, numbers it
+//         -> Similar to Data structures in that we can manipulate them.
+//         -> Array of integers
+
+// Big O of common operations
+// 1. Read the character at a given index, Strings -> T, S = O(1)
+// 2. Traverse, Strings -> T = O(n), S = O(1)
+// Copy, Strings -> T, S = O(n)
+// Append, Strings -> T = O(n)
+// Concatenate, Strings -> T, S = O(n+m)
