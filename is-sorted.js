f4f7cafae24f0d37b1aed901c924484067a0e1c3
abdulsagheer <abdulsagheeras29@gmail.com>
2022-07-01 21:21:07 +0530

    Implemented naive methodology

---
+++ is-sorted.js
@@ -0,0 +1,14 @@
+// Naive method
+
+const isSorted = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] < arr[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+console.log(isSorted([1, 0, 2, 3, 4, 5]));
