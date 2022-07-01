c79bfd55209f08b8af33e23e3a969c72bc6a048a
abdulsagheer <abdulsagheeras29@gmail.com>
2022-07-01 22:44:33 +0530

    Efficient solution for second-largest

---
+++ arrays/second-largest-in-an-array.js
@@ -0,0 +1,31 @@
+// Naive implementation
+
+// const secLar = arr => {
+//     let largest = 0;
+//     for (let i = 1; i < arr.length; i++) {
+//         if(arr[i] > arr[largest]) {
+//             largest = i;
+//         }
+//     }
+//     return largest;
+// }
+
+// console.log(secLar([5, 20,12, 20]))
+
+const secLar = arr => {
+    let res = 1,  lar = 0;
+    for (let i = 1; i < arr.length; i++) {
+        if(arr[i] > arr[lar]) {
+            res = lar;
+            lar = i;
+        }
+        else if(arr[i] != arr[lar]) {
+            res = i;
+        }
+    }
+    return res;
+}
+
+console.log(secLar([5, 20,12, 20]))
+
+// Time complexity - O(n2)
