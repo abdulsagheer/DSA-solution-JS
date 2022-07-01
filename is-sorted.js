0615957a86001a8808743d5077b92d92022f0354
abdulsagheer <abdulsagheeras29@gmail.com>
2022-07-01 21:37:33 +0530

    Efficient method of sorting

--- is-sorted.js
+++ is-sorted.js
@@ -1,14 +1,31 @@
 // Naive method
 
+// const isSorted = (arr) => {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length; j++) {
+//       if (arr[j] < arr[i]) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+// };
+
+// console.log(isSorted([1, 0, 2, 3, 4, 5]));
+
+// Time complexity - O(n2)
+
+// Efficient Implementation
+
 const isSorted = (arr) => {
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[j] < arr[i]) {
-        return false;
-      }
+    if(arr[i] < arr[i - 1]) {
+      return false;    
     }
-    return true;
   }
-};
+return true;
+}
 
 console.log(isSorted([1, 0, 2, 3, 4, 5]));
+
+// Time complexity - O(n)
