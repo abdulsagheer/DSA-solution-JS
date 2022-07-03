a06c50e2aa714f475cf6ec8c0b22e23ed1bb5d42
abdulsagheer <abdulsagheeras29@gmail.com>
2022-07-03 21:39:07 +0530

    Solved duplicate removed problem

---
+++ arrays/remove-duplicates-from-arrays.js
@@ -0,0 +1,14 @@
+// Naive implementation
+
+const remDub = arr => {
+    let res = 1;
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] != arr[res-1]) {
+            if(arr[res] = arr[i]);
+            res++;
+        }
+    }
+    return res;
+}
+
+console.log(remDub([10, 20, 20, 30, 30, 30, 30, 30, 30, 30, 30]));
