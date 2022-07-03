db0a8ad6e9f1f08ccd594060247147a46ec8ee33
abdulsagheer <abdulsagheeras29@gmail.com>
2022-07-03 22:10:39 +0530

    Solved move all zero to end array problem

---
+++ arrays/move-all-0's-to-end.js
@@ -0,0 +1,34 @@
+// Naive implementation
+
+const moveZero = arr => {
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] === 0) {
+            for (let j = 0; j < arr.length; j++) {
+                if(arr[j] != 0) {
+                    swap(arr[j], arr[j]);
+                }
+            }
+        }
+    }
+}
+
+console.log(moveZero([10, 8, 0, 0, 2, 0]));
+
+// Time complexity - O(n2)
+
+// Efficient implementation
+
+const moveZeros = arr => {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i]!=0) {
+            swap(arr[i], arr[count]);
+            count++;
+        }
+    }
+}
+console.log(moveZeros([10, 8, 0, 0, 2, 0]));
+
+
+// Time complexity - O(1)
+
