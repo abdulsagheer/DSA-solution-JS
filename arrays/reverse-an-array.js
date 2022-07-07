c1cafd6bf0d6567a7cb013b252c6ca7ca0548ba6
abdulsagheer <abdulsagheeras29@gmail.com>
2022-07-07 14:59:02 +0530

    Added an extra solution to reverse an array

--- arrays/reverse-an-array.js
+++ arrays/reverse-an-array.js
@@ -8,8 +8,16 @@ const reverse = arr => {
         high--;
     }
 }
+function reverseArr(arr) {
+  var ret = new Array();
+  for (var i = arr.length - 1; i >= 0; i--) {
+    ret.push(arr[i]);
+  }
+  return ret;
+}
 
-console.log(reverse([10,20,30,40,50]))
+var a = [3, 5, 7, 8];
+console.log(reverseArr([10, 20, 30, 40, 50]));
 
 // Time complexity: theta(n)
 // Space complexity: theta(1)
