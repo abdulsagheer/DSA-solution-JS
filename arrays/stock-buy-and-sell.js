00738b8be7b8deeb80785d7ec761256718692823
abdulsagheer <abdulsagheeras29@gmail.com>
2022-07-13 22:42:40 +0530

    Efficient solution for maximum profit from stocks

--- arrays/stock-buy-and-sell.js
+++ arrays/stock-buy-and-sell.js
@@ -23,3 +23,16 @@ const stockBuySell = price => {
 console.log(stockBuySell([100, 180, 260, 310, 40, 535, 695]));
 
 // Efficient Implementation
+
+const maxProfit = price => {
+    let profit = 0;
+    for (let i = 0; i < price.length; i++) {
+      if (price[i] > price[i - 1]) {
+        profit += price[i] - price[i - 1];
+      }
+    }
+    return profit;
+}
+
+console.log(maxProfit([1, 5, 3, 8, 12]));
+
