08934f13f3eec7577f8bbcc6fdab45614245931c
abdulsagheer <abdulsagheeras29@gmail.com>
2022-07-17 21:54:04 +0530

    Solved kth max and min problem solution

---
+++ arrays/find-kth-max-and-min.js
@@ -0,0 +1,55 @@
+// One liner solution --
+var findKthLargest = function (nums, k) {
+  return nums.sort((a, b) => a - b)[nums.length - k];
+};
+
+function FirstKelements(arr, size, k) {
+  // Creating Min Heap for given
+  // array with only k elements
+  // Create min heap with priority queue
+  var minHeap = [];
+  for (var i = 0; i < k; i++) {
+    minHeap.push(arr[i]);
+  }
+
+  // Loop For each element in array
+  // after the kth element
+  for (var i = k; i < size; i++) {
+    minHeap.sort((a, b) => a - b);
+
+    // If current element is smaller
+    // than minimum ((top element of
+    // the minHeap) element, do nothing
+    // and continue to next element
+    if (minHeap[minHeap.length - 3] > arr[i]) continue;
+    // Otherwise Change minimum element
+    // (top element of the minHeap) to
+    // current element by polling out
+    // the top element of the minHeap
+    else {
+      minHeap.reverse();
+      minHeap.pop();
+      minHeap.reverse();
+      minHeap.push(arr[i]);
+    }
+  }
+
+  // Now min heap contains k maximum
+  // elements, Iterate and print
+  for (var iterator of minHeap) {
+    document.write(iterator + " ");
+  }
+}
+
+// Using priority queue  --
+var findKthLargest = function (nums, k) {
+  let heapQueue = new MaxPriorityQueue();
+  nums.forEach((num) => heapQueue.enqueue(num));
+  while (k > 1) {
+    heapQueue.dequeue();
+    k--;
+  }
+  return heapQueue.front().element;
+};
+
+// Time complexity - O(1)
